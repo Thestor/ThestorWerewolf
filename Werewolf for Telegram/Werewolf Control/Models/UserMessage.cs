11dfce5c890deda7d65903c1797544d5c541d76c
Matmail <33591897+Matmail@users.noreply.github.com>
2018-02-10 13:00:27 +0700

    Add files via upload

---
+++ Werewolf for Telegram/Werewolf Control/Models/UserMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Werewolf_Control.Models
+{
+    class UserMessage
+    {
+        public DateTime Time { get; set; }
+        public string Command { get; set; }
+        public bool Replied { get; set; }
+
+        public UserMessage(Message m)
+        {
+            Time = m.Date;
+            Command = m.Text;
+        }
+    }
+}
